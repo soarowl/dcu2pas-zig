917c7bb25d11ca374fe55a1f19811272e28bea95
Zhuo Nengwen <soarowl@yeah.net>
2023-12-22 19:42:27 +0800

    Can read file now.

--- src/main.zig
+++ src/main.zig
@@ -34,4 +34,22 @@ pub fn main() !void {
 fn run_decompile() !void {
     const c = &config;
     std.debug.print("Decompiling {s}...\n", .{c.file});
+    try decompile_file(c.file);
+    std.debug.print("Done.\n", .{});
+}
+
+fn decompile_file(fileName: []const u8) !void {
+    // Open the file
+    const file = try std.fs.openFileAbsolute(fileName, .{ .mode = .read_only });
+    defer file.close();
+
+    // Read the contents
+    const buffer_size = 4 * 1024 * 1024 * 1024;
+    const file_buffer = try file.readToEndAlloc(allocator, buffer_size);
+    defer allocator.free(file_buffer);
+    try decomiple_buffer(file_buffer);
+}
+
+fn decomiple_buffer(buffer: []const u8) !void {
+    std.debug.print("File Size: {}\n", .{buffer.len});
 }
